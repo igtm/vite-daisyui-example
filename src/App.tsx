d664ea98cd14dbc925fd685a9b8b679ef5d5c16b
igtm <github@igtm.link>
2023-05-28 01:23:29 +0900

    init tailwind

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p className="">Click on the Vite and React logos to learn more</p>
+      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        ...
+      </button>
+    </>
+  );
+}
+
+export default App;
